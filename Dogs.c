108c2dbdf844112047f5460f152388c501d61aa7
jssz-Charlie <jsszwcl@outlook.com>
2026-02-28 01:08:33 +0800

    How many dogs?

---
+++ Dogs.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main(void){
+    int dogs;
+    printf("How many dogs do you have? \n");
+    scanf("%d", &dogs);
+    printf("You have %d dogs! \n", dogs);
+    return 0;
+}
